54500bb310a0e5976960cbefa7f7cd86b348f828
HedgeHao <hedgehao@gmail.com>
2021-10-24 22:28:22 +0800

    draw triangle

--- external_texture/linux/include/external_texture/opengl_renderer.h
+++ external_texture/linux/include/external_texture/opengl_renderer.h
@@ -1,28 +1,85 @@
 #ifndef OPENGL_RENDERER_FLUTTER_H
 #define OPENGL_RENDERER_FLUTTER_H
+#include <GL/glew.h>
 #include <gtk/gtk.h>
-#include <GL/gl.h>
 #include <vector>
 
 class OpenGLRenderer {
-    GdkGLContext* context;
-public:
-    OpenGLRenderer(GdkGLContext* context) {
-        this->context = context;
-    }
+  GdkGLContext* context;
 
-    int genTexture(int width, int height) {
-        std::vector<uint8_t> buffer;
-        for (int i=0; i<width*height*3; i++) {
-            buffer.push_back(i%255);
-        }
-        unsigned int texture;
-        glGenTextures(1, &texture);
-        glBindTexture(GL_TEXTURE_2D, texture); 
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, buffer.data());
-        
-        return texture;
+ public:
+  OpenGLRenderer(GdkGLContext* context) { this->context = context; }
+
+  int genTexture(int width, int height) {
+    if (glewInit() != GLEW_OK) {
+      printf("Failed to init GLEW\n");
+      return -1;
     }
+    glViewport(0, 0, width, height);
+
+    float vertices[] = {-0.5f, -0.5f, 0.0f, 0.5f, -0.5f,
+                        0.0f,  0.0f,  0.5f, 0.0f};
+
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    const char* vertexShaderSource =
+        "#version 330 core\n"
+        "layout (location = 0) in vec3 aPos;\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+        "}\0";
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    const char* fragmentShaderSource =
+        "#version 330 core\n"
+        "out vec4 FragColor;\n"
+        "void main()\n"
+        "{\n"
+        "    FragColor = vec4(1.0f, 0.0f, 0.0f, 1.0f);\n"
+        "}\0";
+
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
+                          (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBindVertexArray(VAO);
+    glUseProgram(shaderProgram);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    uint8_t buffer[width * height * 4];
+    glReadPixels(0, 0, width, height, GL_RGBA, GL_UNSIGNED_BYTE, buffer);
+
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA,
+                 GL_UNSIGNED_BYTE, buffer);
+
+    return texture;
+  }
 };
 
-#endif //OPENGL_RENDERER_FLUTTER_H
+#endif  // OPENGL_RENDERER_FLUTTER_H
